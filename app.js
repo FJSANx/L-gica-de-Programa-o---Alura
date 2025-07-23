8a097608966d98bd067f2b6a872210330ba8af4f
FJSAN <fjsan2010@gmail.com>
2025-07-22 21:47:15 -0300

    Terceiro Commit - Adição de Math random

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-{alert('Boas-vindas ao Jogo do Número Secreto!');
-let numeroSecreto = 9;
+alert('Boas-vindas ao Jogo do Número Secreto!');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(`O número secreto é ${numeroSecreto}`);
-let chute
+let chute;
+let tentativas = 1;
 
 //Adicionando um While
 while (chute != numeroSecreto){
-    chute = prompt("Digite um número entre 0 e 10:");
+    chute = prompt("Digite um número entre 0 e 100:");
 //Se o chute for igual ao número secreto
     if (numeroSecreto == chute) {
-        alert(`Parabéns! Você acertou o número secreto! O número era ${numeroSecreto}.`);
-}
+        //Parar o While
+    break;
+        }
 //Se o chute for diferente do número secreto
     else {
         if(numeroSecreto > chute) {
             alert(`Que pena! Você errou. O número secreto é maior que ${chute}.`);
 }       else {
             alert(`Que pena! Você errou. O número secreto é menor que ${chute}.`);
-} }
-}
-}
+            } 
+        tentativas++;
+        }       
+    }
+    
+    let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+    alert(`Parabéns! Você acertou o número secreto! O número era ${numeroSecreto} e você levou ${tentativas} ${palavraTentativas}.`);
